d2bfa0410de5ab50d5d69f76e0224b4777aa2f75
alvannnnn <alvantwent@gmail.com>
2022-12-30 16:54:58 +0700

    memisahkan code index dan dashboard yang sebelumnya jadi 1

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,14 +1,21 @@
-<!-- /.container-fluid -->
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="<?= base_url('assets/img/profile/') . $user['foto_user']; ?>" class="img-fluid rounded-start">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $user['nama_user']; ?></h5>
+                    <p class="card-text"><?= $user['email']; ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
-<!-- End of Main Content -->
-
-<!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
-
-
-</div>
-<!-- /.container-fluid -->
-
-</div>
-<!-- End of Main Content -->
